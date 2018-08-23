20659d7da52e6802e8034e358e694bba0a9b5157
ignacy <ignacy.buczek@onet.pl>
2018-08-23 15:28:15 +0200

    started connecting recyclerview to dao

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.example.shoppinglist
 
-import android.support.v7.app.AppCompatActivity
+import android.arch.persistence.room.Room
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import com.example.shoppinglist.sql.AppDatabase
+import com.example.shoppinglist.sql.LiveDataTestUtil.getValue
+import com.facebook.stetho.Stetho
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.shoppinglist_item.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val testList = arrayListOf("mleko", "cukier", "chleb")
+        Stetho.initializeWithDefaults(this);
+
+
+        val dao = Room.databaseBuilder(this, AppDatabase::class.java, "ShoppingList").allowMainThreadQueries().build()
+        val shoppingLists = getValue(dao.listDao().fetch())
+        //TODO tutaj się wywala, nie mam pojęcia czemu
+        val ids = shoppingLists.map { it.toString() }.toSet()
         rc_shopping_list.layoutManager = LinearLayoutManager(this)
-        rc_shopping_list.adapter = ShoppingAdapter(testList, this)
+        rc_shopping_list.adapter = ShoppingAdapter(ArrayList(ids), this)
+
+
+
+        fab.setOnClickListener {
+            dao.listDao().persist(ShoppingList(0, 0, listOf(ShoppingItem("cukier")), false))
+        }
 
     }
 }
