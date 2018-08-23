7947726108eb325a1659fc2b5d53b5f56e7633c5
ignacy <ignacy.buczek@onet.pl>
2018-08-23 13:54:42 +0200

    added simple recyclerview

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.shoppinglist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.shoppinglist_item.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val testList = arrayListOf("mleko", "cukier", "chleb")
+        rc_shopping_list.layoutManager = LinearLayoutManager(this)
+        rc_shopping_list.adapter = ShoppingAdapter(testList, this)
+
     }
 }
---
+++ app/src/main/java/com/example/shoppinglist/ShoppingAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.shoppinglist
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.shoppinglist_item.view.*
+
+/**
+ * Created by ignacy on 23.08.18.
+ */
+
+class ShoppingAdapter(val items: ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+    // Gets the number of animals in the list
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    // Inflates the item views
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.shoppinglist_item, parent, false))
+    }
+
+    // Binds each animal in the ArrayList to a view
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.product?.text = items.get(position)
+    }
+}
+
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    // Holds the TextView that will add each animal to
+    val product = view.shoppinglist_item
+}
